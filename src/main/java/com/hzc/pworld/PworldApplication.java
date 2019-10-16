3f74cf8e2317c2c26ae2370b6f0353117d5a1114
xwb15016315961 <2199084052@qq.com>
2019-10-16 13:15:00 +0800

    init repo

---
+++ src/main/java/com/hzc/pworld/PworldApplication.java
@@ -0,0 +1,13 @@
+package com.hzc.pworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PworldApplication.class, args);
+    }
+
+}
